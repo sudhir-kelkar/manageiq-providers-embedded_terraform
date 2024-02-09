183b9377f3724a7774e8d8d905c8a2f50038c010
Adam Grare <adam@grare.com>
2024-02-09 10:23:47 -0500

    Add EmbeddedTerraform AutomationManager

---
+++ app/models/manageiq/providers/embedded_terraform/automation_manager.rb
@@ -0,0 +1,16 @@
+class ManageIQ::Providers::EmbeddedTerraform::AutomationManager < ManageIQ::Providers::EmbeddedAutomationManager
+  supports_not :refresh_ems
+
+  def self.hostname_required?
+    # TODO: ExtManagementSystem is validating this
+    false
+  end
+
+  def self.ems_type
+    @ems_type ||= "embedded_terraform".freeze
+  end
+
+  def self.description
+    @description ||= "Embedded Terraform".freeze
+  end
+end
--- lib/manageiq/providers/embedded_terraform.rb
+++ lib/manageiq/providers/embedded_terraform.rb
@@ -1,2 +1,15 @@
 require "manageiq/providers/embedded_terraform/engine"
 require "manageiq/providers/embedded_terraform/version"
+
+module ManageIQ
+  module Providers
+    module EmbeddedTerraform
+      def self.seed
+        ManageIQ::Providers::EmbeddedTerraform::AutomationManager.in_my_region.first_or_create!(
+          :name => "Embedded Terraform",
+          :zone => MiqServer.my_server.zone
+        )
+      end
+    end
+  end
+end
