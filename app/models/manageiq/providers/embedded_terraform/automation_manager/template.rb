a7500ead426e242462203857f6a5e47cdf6c17b3
Adam Grare <adam@grare.com>
2024-04-24 12:54:41 -0400

    Pass :env and :credential to create_job

--- app/models/manageiq/providers/embedded_terraform/automation_manager/template.rb
+++ app/models/manageiq/providers/embedded_terraform/automation_manager/template.rb
@@ -1,10 +1,10 @@
 class ManageIQ::Providers::EmbeddedTerraform::AutomationManager::Template < ManageIQ::Providers::EmbeddedAutomationManager::ConfigurationScriptPayload
   has_many :stacks, :class_name => "ManageIQ::Providers::EmbeddedTerraform::AutomationManager::Stack", :foreign_key => :configuration_script_base_id, :inverse_of => :configuration_script_payload, :dependent => :nullify
 
-  def run(input_vars = {}, _userid = nil)
-    env_vars = {}
-    credentials = []
+  def run(vars = {}, _userid = nil)
+    env_vars    = vars.delete(:env)        || {}
+    credentials = vars.delete(:credential) || []
 
-    self.class.module_parent::Job.create_job(self, env_vars, input_vars, credentials).tap(&:signal_start)
+    self.class.module_parent::Job.create_job(self, env_vars, vars, credentials).tap(&:signal_start)
   end
 end
