595ae67d7fce9402bb309a3172d0232d1aced86c
Adam Grare <adam@grare.com>
2024-05-24 10:32:44 -0400

    Pass a certs volume to opentofu-runner

--- app/models/opentofu_worker.rb
+++ app/models/opentofu_worker.rb
@@ -57,6 +57,7 @@ class OpentofuWorker < MiqWorker
 
   def enable_systemd_unit
     super
+    create_tls_certs
     create_podman_secret
   end
 
@@ -79,6 +80,9 @@ class OpentofuWorker < MiqWorker
       definition[:spec][:template][:spec][:containers].first[:volumeMounts] << {:name => "cert-path", :mountPath => "/opt/app-root/src/config/cert"}
       definition[:spec][:template][:spec][:volumes] << {:name => "cert-path", :secret => {:secretName => ENV["API_SSL_SECRET_NAME"], :items => [{:key => "tf_runner_crt", :path => "tls.crt"}, {:key => "tf_runner_key", :path => "tls.key"}], :defaultMode => 420}}
     end
+  end
+
+  def create_tls_certs
   end
 
   def create_podman_secret
