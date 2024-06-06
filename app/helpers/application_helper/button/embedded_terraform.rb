27791555e659c82d05d51342fcfe525b604b0305
Adam Grare <adam@grare.com>
2024-06-06 13:13:24 -0400

    Check embedded_terraform role in Button disabled?
    Prevent the user from adding credentials/repositories if the
    embedded_terraform role is disabled.

--- app/helpers/application_helper/button/embedded_terraform.rb
+++ app/helpers/application_helper/button/embedded_terraform.rb
@@ -1,7 +1,10 @@
 class ApplicationHelper::Button::EmbeddedTerraform < ApplicationHelper::Button::Basic
   def disabled?
-    if Rbac.filtered(ManageIQ::Providers::EmbeddedTerraform::AutomationManager.all).empty?
+    if !MiqRegion.my_region.role_active?('embedded_terraform')
+      @error_message = _("Embedded Terraform Role is not enabled.")
+    elsif Rbac.filtered(ManageIQ::Providers::EmbeddedTerraform::AutomationManager.all).empty?
       @error_message = _("User isn't allowed to use the Embedded Terraform provider.")
     end
+    @error_message.present?
   end
 end
