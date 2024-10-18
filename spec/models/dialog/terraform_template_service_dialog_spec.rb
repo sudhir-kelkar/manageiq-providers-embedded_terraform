cfb764708e17fb9a636a571fbbd5c77f236541be
MANOJ PUTHRAN <putmanoj@in.ibm.com>
2024-10-19 02:32:01 +0530

    add test for create dialog - when no template nor extra variables

--- spec/models/dialog/terraform_template_service_dialog_spec.rb
+++ spec/models/dialog/terraform_template_service_dialog_spec.rb
@@ -1,8 +1,8 @@
 RSpec.describe Dialog::TerraformTemplateServiceDialog do
   describe "#create_dialog" do
-    context "with no template input vars, but with extra vars" do
-      let(:terraform_template_with_no_input_vars) { nil }
+    let(:terraform_template_with_no_input_vars) { nil }
 
+    context "with no template input vars, but with extra vars" do
       it "creates a dialog with extra variables" do
         extra_vars = {
           'some_extra_var'  => {:default => 'blah'},
@@ -13,23 +13,41 @@ RSpec.describe Dialog::TerraformTemplateServiceDialog do
         dialog = subject.create_dialog("mydialog1", terraform_template_with_no_input_vars, extra_vars)
         expect(dialog).to have_attributes(:label => 'mydialog1', :buttons => "submit,cancel")
 
-        tabs = dialog.dialog_tabs
-        expect(tabs.size).to eq(1)
-
-        tab0 = tabs[0]
-        assert_tab_attributes(tab0)
-
-        groups = tab0.dialog_groups
-        expect(groups.size).to eq(1)
-
-        assert_extra_variables_group(groups[0])
-      end
-
-      it "creates a dialog with no extra variables" do
-        dialog = described_class.create_dialog("mydialog2", terraform_template_with_no_input_vars, {})
-        expect(dialog.dialog_tabs[0].dialog_groups.size).to eq(1)
+        group = assert_default_tab(dialog)
+        assert_extra_variables_group(group)
       end
     end
+  end
+
+  context "with no template input vars, nor extra vars" do
+    it "creates a dialog with place-holder variable argument, when nil template & empty extra vars" do
+      dialog_label = "mydialog2"
+      dialog = described_class.create_dialog(dialog_label, nil, {})
+
+      group = assert_default_tab(dialog)
+      assert_default_variables_group(group, dialog_label)
+    end
+
+    it "creates a dialog with place-holder variable argument, when nil template & nil extra vars" do
+      dialog_label = "mydialog3"
+      dialog = described_class.create_dialog(dialog_label, nil, nil)
+
+      group = assert_default_tab(dialog)
+      assert_default_variables_group(group, dialog_label)
+    end
+  end
+
+  def assert_default_tab(dialog)
+    tabs = dialog.dialog_tabs
+    expect(tabs.size).to eq(1)
+
+    tab0 = tabs[0]
+    assert_tab_attributes(tab0)
+
+    groups = tab0.dialog_groups
+    expect(groups.size).to eq(1)
+
+    groups[0]
   end
 
   def assert_tab_attributes(tab)
@@ -50,5 +68,14 @@ RSpec.describe Dialog::TerraformTemplateServiceDialog do
     assert_field(fields[0], DialogFieldTextBox, :name => 'some_extra_var', :default_value => 'blah', :data_type => 'string')
     assert_field(fields[1], DialogFieldTextBox, :name => 'other_extra_var', :default_value => '{"name":"some_value"}', :data_type => 'string')
     assert_field(fields[2], DialogFieldTextBox, :name => 'array_extra_var', :default_value => '[{"name":"some_value"}]', :data_type => 'string')
+  end
+
+  def assert_default_variables_group(group, field_value)
+    expect(group).to have_attributes(:label => "Variables", :display => "edit")
+
+    fields = group.dialog_fields
+    expect(fields.size).to eq(1)
+
+    assert_field(fields[0], DialogFieldTextBox, :name => 'name', :default_value => field_value, :data_type => 'string')
   end
 end
